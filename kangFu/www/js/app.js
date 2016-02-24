274b2a5bf33f7b0c904ef526faf950c21f3b0666
JohnnyTam <shrimpcolo@gmail.com>
2016-02-24 16:00:56 +0800

    [BugFix]
    [Feature] 引入ngCordova，并注入到app.js中。
    
    [Descripton]
    1) bower install ngCordova --save
    2) angular.module(... [..., ngCordova ...
    3)  <script src="lib/ngCordova/dist/ng-cordova.js"></script>

--- kangFu/www/js/app.js
+++ kangFu/www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('kangFu', ['ionic', 'kangFu.controllers', 'kangFu.services'])
+angular.module('kangFu', ['ionic', 'ngCordova', 'kangFu.controllers', 'kangFu.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
